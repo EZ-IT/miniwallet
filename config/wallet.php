e699a3a28ab06927b1940611e3f024ca03e1550c
Zoltan Fabian <fszotyi@gmail.com>
2025-09-27 16:18:33 +0300

    Added wallet configuration for transaction commission rate

---
+++ config/wallet.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    /*
+    * This value represents the percentage fee charged on every outgoing
+    * transaction. For example, a value of 0.015 corresponds to a 1.5% fee.
+    *
+    */
+    'commission_rate' => 0.015,
+];
