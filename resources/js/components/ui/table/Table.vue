652b55e54d40d7c5fb7837cd45879f796c4439b9
Zoltan Fabian <fszotyi@gmail.com>
2025-09-27 16:31:33 +0300

    Published reusable components for UI table elements from shadcn and sonner

---
+++ resources/js/components/ui/table/Table.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { cn } from "@/lib/utils";
+
+const props = defineProps({
+  class: { type: null, required: false },
+});
+</script>
+
+<template>
+  <table
+      data-slot="table"
+      :class="cn('w-full caption-bottom text-sm', props.class)"
+    >
+      <slot />
+    </table>
+</template>
